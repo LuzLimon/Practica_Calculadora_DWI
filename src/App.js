328e376e1cb71564613ecb5b4ebcee2ff9395201
LuzLimon <luzelenalimondelacruz123@gmail.com>
2022-06-12 22:38:50 -0500

    feat: New functionality is added -calculator added -added, subtract, divide and multiply functions to our calculator

--- src/App.js
+++ src/App.js
@@ -21,16 +21,16 @@ function App() {
   const [resultado, setresultado] = useState("");
 
   const sumar = function (e) {
-    setResult(Number(primerNum) + Number(segundoNum));
+    setresultado(Number(primerNum) + Number(segundoNum));
   };
   const restar = function (e) {
-    setResult(Number(primerNum) - Number(segundoNum));
+    setresultado(Number(primerNum) - Number(segundoNum));
   };
   const dividir = function (e) {
-    setResult(Number(primerNum) / Number(segundoNum));
+    setresultado(Number(primerNum) / Number(segundoNum));
   };
   const multiplicar = function (e) {
-    setResult(Number(primerNum) * Number(segundoNum));
+    setresultado(Number(primerNum) * Number(segundoNum));
   };
 
 return (
@@ -65,7 +65,44 @@ return (
         <li id={idx}>{`${person.name} ${person.lastName}`}</li>
       ))}
     </ul>
-   
+    <label for="primerNum">Ingresar primer dígito:
+      </label>
+      <input
+        type="number"
+        name="primerNum"
+        onChange={(event) => {
+          setprimerNum(event.target.value);
+        }}
+      />
+      <br/>
+      <br/>
+      <label for="primerNum">Ingresar Segundo dígito:
+      </label>
+      <input
+        type="number"
+        name="segundoNum"
+        onChange={(event) => {
+          setsegundoNum(event.target.value);
+        }}
+      />
+      <br/>
+      <br/>
+      <div class="buttons">
+        <button className="btn" type="button" class="btn btn-primary" onClick={sumar}>Sumar</button>
+        <button className="btn" type="button" class="btn btn-warning" onClick={restar}>Restar</button>
+        <button className="btn" type="button" class="btn btn-primary" onClick={dividir}>Dividir</button>
+        <button className="btn" type="button" class="btn btn-primary" onClick={multiplicar}>Multiplicar</button>
+      </div>
+      <br/>
+      <br/>
+      <label for="primerNum">Resultado:
+      </label>
+      <input
+        type="text"
+        name="results"
+        value={resultado}
+        disabled
+      />
     </div>
   );
 }
